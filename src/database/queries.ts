12d6ef43bfed0d5d6af07675ed8df8a4a274612f
jservaes <jservaes@umass.edu>
2026-04-06 20:16:01 -0400

    General Queries

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,3 +1,21 @@
+import { Client } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log("Starting database connection...");
+
+const client = new Client({
+    host: process.env.CLIENT_HOST,
+    port: 5432,
+    database: process.env.CLIENT_DATABASE,
+    user: process.env.CLIENT_USER,
+    password: process.env.CLIENT_PASSWORD,
+});
+
+await client.connect();
+
+console.log("Connected to database");
+
 interface Address {
     street: string;
     city: string;
@@ -10,42 +28,107 @@ interface Address {
  * @param email The user's email address. This should be unique across all users in the database and should be validated by the caller before being passed to this function.
  * @param name The user's name. This is an optional field and can be null if the user does not wish to provide it.
  */
-async function add_user(email: string, name?: string) {};
+async function add_user(email: string, name?: string) {
+    const result = await client.query(
+        `INSERT INTO users (email,name)
+        VALUES ($1, $2)
+        RETURNING *;`,
+        [email,name ?? null]
+    );
+    return result.rows[0];
+};
 
-async function get_user_by_email(email: string) {};
+async function get_user_by_email(email: string) {
+    const result = await client.query(
+        `SELECT * 
+        FROM users
+        WHERE email = $1;`,
+        [email]
+    );
+    return result.rows[0] ?? null;
+};
 
 /**
  * Saves a new store to the database.
  * @param source The source string of the store.
  * @param url The URL of the store's website.
  */
-async function add_store(source: string, url: string,) {};
+async function add_store(source: string, url: string,) {
+    const result = await client.query(
+        `INSERT INTO stores(source,url)
+         VALUES ($1, $2)
+         RETURNING *;`,
+         [source, url]
+    );
+    return result.rows[0];
+};
 
-async function get_store_by_source(source: string) {};
+async function get_store_by_source(source: string) {
+    const result = await client.query(
+        `SELECT * 
+        FROM stores
+        WHERE store_source = $1;`,
+        [source]
+    );
+    return result.rows[0] ?? null;
+};
 
 /**
  * Adds a new location for a store to the database.
  * @param source The source string of the store. This should match the source string used when adding the store to the database.
  * @param address The address of the store location. This should be an object including the street, city, state, and zip_code of the location.
  */
-async function add_store_location(source: string, address: Address) {};
+async function add_store_location(source: string, address: Address) {
+    const full_address = `${address.street}, ${address.city}, ${address.state}, ${address.zip_code}`;
+    const result = await client.query(
+        `INSERT INTO store_addresses(store_source, address)
+        VALUES ($1, $2)
+        RETURNING *;`,
+        [source, full_address]
+    );
+    return result.rows[0];
+};
 
 /**
  * Adds a new brand to the database. A brand represents a specific company or manufacturer that produces products that can be sold at stores.
  * @param name 
  */
-async function _add_brand(name: string) {};
+async function _add_brand(name: string) {
+    const result = await client.query(
+        `INSERT INTO brands (name)
+        VALUES ($1)
+        RETURNING *;`,
+        [name]
+    );
+    return result.rows[0];
+};
 
 /**
  * Adds a new category to the database. A category represents a specific type of product, such as "dairy" or "snacks", that can be used to group products together.
  * @param name
  */
-async function _add_category(name: string) {};
+async function _add_category(name: string) {
+    const result = await client.query(
+        `INSERT INTO categories (name)
+        VALUES($1)
+        RETURNING *;`,
+        [name]
+    );
+    return result.rows[0];
+};
 
 /** Adds a new unit of measurement to the database. A unit represents a specific way of measuring the quantity of a product, such as "ounce" or "pound".
  * @param name 
  */
-async function _add_unit(name: string) {};
+async function _add_unit(name: string, unit_type: string) {
+    const result = await client.query(
+        `INSERT INTO units (name, unit_type)
+        VALUES ($1, $2)
+        RETURNING *;`,
+        [name, unit_type]
+    );
+    return result.rows[0];
+};
 
 /**
  * Adds a new product to the database. A product represents a specific type of item that can be sold at multiple stores, and includes information about the brand, category, unit of measurement, and whether the product is eligible for purchase with EBT benefits.
@@ -55,10 +138,40 @@ async function _add_unit(name: string) {};
  * @param unit_id 
  * @param ebt_elligible 
  */
-async function add_product(name: string, brand_id?: number, category_id?: number, unit_id?: number, ebt_elligible?:boolean) {};
+async function add_product(name: string, brand_id?: number, category_id?: number, unit_id?: number, ebt_eligible?:boolean) {
+    const result = await client.query(
+        `INSERT INTO products (name, brand_id, category_id, unit_id. ebt_eligible)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *;`,
+        [
+            name,
+            brand_id ?? null,
+            category_id ?? null, 
+            unit_id ?? null,
+            ebt_eligible ?? false
+        ]
+    );
+    return result.rows[0];
+};
 
-async function get_product_by_id(product_id: number) {};
-async function get_product_by_name(name: string) {};
+async function get_product_by_id(product_id: number) {
+    const result = await client.query(
+        `SELECT *
+        FROM products
+        WHERE product_id = $1;`,
+        [product_id]
+    );
+    return result.rows[0] ?? null;
+};
+async function get_product_by_name(name: string) {
+    const result = await client.query(
+        `SELECT *
+        FROM products
+        WHERE name = $1;`,
+        [name]
+    );
+    return result.rows;
+};
 
 /**
  * Adds a new item to the database. An item represents a specific product being sold at a specific store, and includes information about the average rating of the product at that store, the number of ratings, the quantity in a package, and the unit of measurement for the product.
@@ -70,11 +183,52 @@ async function get_product_by_name(name: string) {};
  * @param package_quantity 
  * @param unit_id 
  */
-async function add_item(product_id: number, store_id: number, store_item_id: number, avg_rating?: number, rating_count?: number, package_quantity?: number, unit_id?: number) {};
+async function add_item(product_id: number, store_source: string, store_item_id: number, avg_rating?: number, rating_count?: number, package_quantity?: number, unit_id?: number) {
+    const result = await client.query(
+        `INSERT INTO items
+        (product_id, store_source, store_item_id, avg_rating, rating_count, package_quantity, unit_id)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *;`,
+        [product_id,
+            store_source, 
+            store_item_id, 
+            avg_rating ?? null,
+            rating_count ?? null,
+            package_quantity ?? null,
+            unit_id ?? null
+        ]
+    );
+    return result.rows[0];
+};
 
-async function get_item_by_store_item_id(store_id: number, store_item_id: number) {};
-async function get_item_by_id(item_id: number) {};
-async function get_item_by_name(name: string) {};
+async function get_item_by_store_item_id(store_source: string, store_item_id: number) {
+    const result = await client.query(
+        `SELECT *
+        FROM items
+        WHERE store_source = $1 AND store_item_id = $2;`,
+        [store_source, store_item_id]
+    );
+    return result.rows[0] ?? null; 
+};
+async function get_item_by_id(item_id: number) {
+    const result = await client.query(
+        `SELECT *
+        FROM items
+        WHERE item_id = $1;`, 
+        [item_id]
+    );
+    return result.rows[0] ?? null;
+};
+async function get_item_by_name(name: string) {
+    const result = await client.query(
+        `SELECT items.*
+        FROM items
+        JOIN products ON items.product_id = products.product_id
+        WHERE products.name = $1;`,
+        [name]
+    );
+    return result.rows;
+};
 
 /**
  * Adds a new deal to the database. A deal represents a specific price on an item at a specific store, and includes information about the original price, whether the item is currently on sale, the last time the deal was fetched from the store's website, the sale price (if the item is currently on sale), and whether the sale price is only available to members of the store's loyalty program.
@@ -85,6 +239,30 @@ async function get_item_by_name(name: string) {};
  * @param sale_price 
  * @param membership_sale 
  */
-async function add_deal(item_id: number, original_price: number, on_sale: boolean, last_fetched:Date, sale_price?: number, membership_sale?: boolean) {};
+async function add_deal(item_id: number, original_price: number, on_sale: boolean, last_fetched:Date, sale_price?: number, membership_sale?: boolean) {
+    const result = await client.query(
+        `INSERT INTO deals
+        (item_id, original_price, on_sale, last_fetched, sale_price, membership_sale)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING *;`,
+        [
+            item_id,
+            original_price, 
+            on_sale,
+            last_fetched,
+            sale_price ?? null, 
+            membership_sale ?? false
+        ]
+    );
+    return result.rows[0];
+};
 
-async function get_deal_by_id(deal_id: number) {};
+async function get_deal_by_id(deal_id: number) {
+    const result = await client.query(
+        `SELECT *
+        FROM deals
+        WHERE deal_id = $1;`, 
+        [deal_id]
+    );
+    return result.rows[0] ?? null;
+};
