2846d5140065706dd0dc16e884e6711953a144a7
hyuzukirmizi <hyuzukirmizi@umass.edu>
2026-04-09 14:51:18 -0400

    Haluk backend changes (#6)
    * renaming
    
    * update
    
    * update
    
    * comparison endpoint
    
    * update .gitignore
    
    * deleted modules
    
    * Item id endpoint
    
    * Update .gitignore

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const { getJson } = require('serpapi');
-const helmet = require('helmet');
-const { query, body, validationResult } = require('express-validator');
-const { getBestItems } = require('./comparison_logic_test/comparison');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,19 +25,6 @@ app.get('/api/prices',
     }
     const { product, zipCode } = req.query;
     try {
-<<<<<<< HEAD
-      const response = await getJson({
-        engine: "google_shopping",
-        q: product,
-        location: zipCode || "United States",
-        hl: "en",
-        gl: "us",
-        api_key: process.env.SERPAPI_KEY
-      });
-      const results = (response.shopping_results || [])
-        .filter(item => !blockedStores.some(store =>
-          item.source?.toLowerCase().includes(store.toLowerCase())
-=======
         // Fetch data from SerpApi's Google Shopping engine
         const response = await getJson({
             engine: "google_shopping",
@@ -55,7 +39,6 @@ app.get('/api/prices',
         const results = (response.shopping_results || [])
             .filter(item => !blockedStores.some(store => 
             item.source?.toLowerCase().includes(store.toLowerCase())
->>>>>>> fc75f2672e4dc3746698e305b03a796706165baf
         ));
       searchHistory.push({ product, zipCode, timestamp: new Date() });
       res.json({
@@ -90,8 +73,8 @@ app.post('/api/block',
 
 //Get the blocked stores
 app.get('/api/block', (req, res) => {
-  res.json({ blockedStores });
-});
+    res.json({ blockedStores });
+  });
 
 // Endpoint for comparison logic
 app.get('/api/compare', (req, res) => {
@@ -99,11 +82,5 @@ app.get('/api/compare', (req, res) => {
 });
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-=======
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
->>>>>>> fc75f2672e4dc3746698e305b03a796706165baf
