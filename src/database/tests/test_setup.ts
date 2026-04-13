5986871f9e6e2d7ac4eb5a1924f899cc8a1d4948
jservaes <jservaes@umass.edu>
2026-04-13 18:30:20 -0400

    Details in Pull Request again

--- src/database/tests/test_setup.ts
+++
@@ -1,8 +0,0 @@
-import { execFileSync } from "node:child_process";
-
-// This script is intended to be run before all tests to ensure the database is in a clean state.
-export default async () => {
-    console.log("Running database setup script...");
-    execFileSync("npx", ["supabase", "start"], { stdio: "inherit" });
-    execFileSync("npx", ["supabase", "db", "reset"], { stdio: "inherit" });
-};
--- src/database/tests/test_teardown.ts
+++
@@ -1,6 +0,0 @@
-import { execFileSync } from "node:child_process";
-
-// This script is intended to be run after all tests to clean up the database and stop the Supabase instance.
-export default async () => {
-    execFileSync("npx", ["supabase", "stop"], { stdio: "inherit" });
-}
