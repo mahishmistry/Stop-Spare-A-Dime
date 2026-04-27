05086d2603a967f174748afe71ea6c901a746f90
Dedeep1 <Dsurampudi23@gmail.com>
2026-04-27 15:57:42 -0400

    Fix env variables for Firebase config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -8,14 +8,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCA5bS4RjO4g2Ersq55z6QpuKLoFmdfTJE",
-  authDomain: "stop-spare-a-dime.firebaseapp.com",
-  projectId: "stop-spare-a-dime",
-  storageBucket: "stop-spare-a-dime.firebasestorage.app",
-  messagingSenderId: "375761236980",
-  appId: "1:375761236980:web:55ff4286cb22af5f12bfa1",
-  measurementId: "G-T1TNNE9VRP"
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
